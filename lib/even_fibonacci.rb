070ac4726a36275a32f58bdb566c7d7048eae1ee
ryuichi7 <brettrheenan@gmail.com>
2015-12-02 17:29:34 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,21 +1,33 @@
-def even_fibonacci(n)
-  fib(n).select do |int|
-    int.even? && int < n
-  end
-end
+# def even_fibonacci(n)
+#   fib(n).select do |int|
+#     int.even? && int < n
+#   end
+# end
+
+# def even_fibonacci_sum(n)
+#   even_fibonacci(n).inject(:+)
+# end
+
+
+
+# def fib(n, sequence=[1])
+#   n.times do
+#     current_number, last_number = sequence.last(2)
+#     sequence << current_number + (last_number || 0)
+#   end
+#   sequence
+# end
 
 def even_fibonacci_sum(n)
-  even_fibonacci(n).inject(:+)
-end
+  start = [1,1]
 
-
-
-def fib(n, sequence=[1])
-  n.times do
-    current_number, last_number = sequence.last(2)
-    sequence << current_number + (last_number or 0)
+  while start[-2] + start[-1] < n
+    start << start[-2] + start[-1]
   end
-  sequence
+
+  result = 0
+  start.each { |x| result += x if x.even? }
+  p result
 end
 
 
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,25 +1,43 @@
+# class EvenFibonacci
+
+#   def initialize(n)
+#     @n = n
+#   end
+
+#   def even_fibonacci
+#     fib.select do |int|
+#       int.even? && int < @n
+#     end
+#   end
+
+#   def sum
+#     even_fibonacci.inject(:+)
+#   end
+
+#   def fib(sequence=[1])
+#     @n.times do
+#       current_number, last_number = sequence.last(2)
+#       sequence << current_number + (last_number || 0)
+#     end
+#     sequence
+#   end
+# end
 class EvenFibonacci
 
   def initialize(n)
     @n = n
   end
 
-  def even_fibonacci
-    fib.select do |int|
-      int.even? && int < @n
-    end
-  end
-
   def sum
-    even_fibonacci.inject(:+)
-  end
+    start = [1,1]
 
-  def fib(sequence=[1])
-    @n.times do
-      current_number, last_number = sequence.last(2)
-      sequence << current_number + (last_number or 0)
+    while start[-2] + start[-1] < @n
+      start << start[-2] + start[-1]
     end
-    sequence
+
+    result = 0
+    start.each { |x| result += x if x.even? }
+    p result
   end
 end
 
