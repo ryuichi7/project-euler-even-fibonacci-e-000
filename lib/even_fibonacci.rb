8d16228f9203e5b3f57817a193d4ce4ea8300ebf
ryuichi7 <brettrheenan@gmail.com>
2015-12-02 12:19:19 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,21 @@
-# Implement your procedural solution here!
+def even_fibonacci(n)
+  fib(n).select do |int|
+    int.even? && int < n
+  end
+end
+
+def even_fibonacci_sum(n)
+  even_fibonacci(n).inject(:+)
+end
+
+
+
+def fib(n, sequence=[1])
+  n.times do
+    current_number, last_number = sequence.last(2)
+    sequence << current_number + (last_number or 0)
+  end
+  sequence
+end
+
+
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,25 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(n)
+    @n = n
+  end
+
+  def even_fibonacci
+    fib.select do |int|
+      int.even? && int < @n
+    end
+  end
+
+  def sum
+    even_fibonacci.inject(:+)
+  end
+
+  def fib(sequence=[1])
+    @n.times do
+      current_number, last_number = sequence.last(2)
+      sequence << current_number + (last_number or 0)
+    end
+    sequence
+  end
+end
+
